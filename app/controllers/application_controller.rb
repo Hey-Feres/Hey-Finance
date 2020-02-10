41673f8424146ea2e3d615e0f257fd35e9699650
Hey-Feres <brunoferesvillela@icloud.com>
2020-02-10 14:08:58 -0300

    Update

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,6 @@
 class ApplicationController < ActionController::Base
 #https://codepen.io/amwill/pen/YyexrJ
 #https://codepen.io/edmondko/pen/udcHG
+#https://codepen.io/ahomnky/pen/Wwmwar
+#https://codepen.io/mallendeo/pen/brZJBa
 end
